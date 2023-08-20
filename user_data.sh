6aef073f3c67a9ae24bc708e9d4474d92fb81ca5
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-20 12:50:45 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -5,7 +5,7 @@ sudo yum install zip -y
 
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum install jenkins -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
