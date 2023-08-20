141c531c5c5f7331367bc786193b382269981528
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-20 12:54:20 +0530

    Update _variables.tf

--- _variables.tf
+++ _variables.tf
@@ -6,7 +6,7 @@ variable "subnet_ids" {
 variable "instance_type" {
   description = "The type of instance to start"
   type        = string
-  default     = "t3g.medium"
+  default     = "t4g.medium"
 }
 
 variable "project_name_prefix" {
