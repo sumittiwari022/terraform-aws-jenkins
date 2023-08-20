9b95373157e148e898691cdb5fbbd78b18af0610
Sumit Tiwari <60305045+sumittiwari022@users.noreply.github.com>
2023-08-20 12:46:47 +0530

    port 22 added

--- main.tf
+++ main.tf
@@ -87,6 +87,20 @@ resource "aws_security_group" "jenkins_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    description = "Allow SSM into the server"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${data.aws_vpc.jenkins_vpc.cidr_block}"]
+  }
+  egress {
+    description = "Allow traffic to internet for Package installation"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "ec2" {
